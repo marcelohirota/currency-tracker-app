8e78f28007769756b8e56235606fd65c79842785
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-05-12 18:26:54 +0930

    connection with CoinAPI

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,23 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'CDAA61DB-C618-4EB8-A039-2DE765775678';
+
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    http.Response response = await http.get(requestURL);
+
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+
+      var lastPrice = decodedData['rate'];
+
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
